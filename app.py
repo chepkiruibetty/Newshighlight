919ca28c4ca2310279b809912bb60ce1f63054c9
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-31 23:10:29 +0300

    corrected on spacing and indentation

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ def index():
     topheadlines  = newsapi.get_top_headlines(source="BBC news")
 articles = topheadlines['articles']
 
-description[]
-news[]
-image[]
+    description=[]
+    news=[]
+    image=[]
 
-for i in range(len(articles)):
-    myarticles = [articles[i]]
-    news.append(myarticles['title'])
-    description.append(myarticles['description'])
-    image.append(myarticles)['urlToImage']
+    for i in range(len(articles)):
+        myarticles = [articles[i]]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        image.append(myarticles)['urlToImage']
     
     mylist=zip(news,description,image)
+    
     return render_template('index.html',context = mylist)
 
 if __name__== "__main__":
