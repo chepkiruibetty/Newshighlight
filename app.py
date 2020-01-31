1347053b6c1a1950dcf7f745d10ce42317dbbfa4
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-31 22:19:26 +0300

    add source to the news headlines

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     newsapi =NewsApiClient(api_key='NEWS_API_CLIENT')
+    topheadlines  = newsapi.get_top_headlines(source="BBC news")
+articles = topheadlines['articles']
+desc =
