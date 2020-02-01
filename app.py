8e49ad33740aeb2a4a1bed7eaa4c058c1704f122
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-01 10:52:01 +0300

    add route for bbc news

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+    topheadlines = newsapi.get_top_headlines(sources="ary-news")
 
     articles = topheadlines['articles']
 
@@ -24,6 +24,27 @@ def Index():
     mylist = zip(news, desc, img)
 
     return render_template('index.html', context = mylist)
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
 
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+
+    return render_template('bbc.html', context = mylist)
+    
 if __name__ == "__main__":
     app.run(debug=True)
