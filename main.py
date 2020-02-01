47a3f545c83cd055c71b0c00f3335f0f259a480a
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-01 22:03:48 +0300

    created nbc template file for nbc news

--- main.py
+++ main.py
@@ -56,5 +56,32 @@ def bbc():
 
     return render_template('bbc.html', context = mylist)
     
+@app.route('/fox')
+def fox():
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
+    topheadlines = newsapi.get_top_headlines(sources="fox-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+    url = []
+    publAt = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
+        publAt.append(myarticles['publishedAt'])
+
+    mylist = zip(news, desc, img,url,publAt)
+
+
+    return render_template('fox.html', context = mylist)
+    
 if __name__ == "__main__":
     app.run(debug=True)
