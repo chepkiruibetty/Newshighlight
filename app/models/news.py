075f16eade5eec98f6a282ad32ec05202e8d7ef3
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-03 11:16:38 +0300

    created models file

---
+++ app/models/news.py
@@ -0,0 +1,29 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,name, description, url, category, language, country):
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+
+class Articles:
+    '''
+    Articles class to define Articles Objects
+    '''
+
+    def __init__(self,author, title, description, url, urlToImage, publishedAt, content):
+        
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Projects/NEWS-APP/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/NEWS-APP/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
